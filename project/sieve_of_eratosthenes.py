0f6808aa51cf27c2f77dfe379f29a80e37b593bb
Joseph Brown <josebrwn@gmail.com>
2019-05-24 11:18:32 -0700

    demo count of operations to sieve primes equals O(n log log n)

--- project/sieve_of_eratosthenes.py
+++ project/sieve_of_eratosthenes.py
@@ -4,17 +4,25 @@ def find_primes(max_value):
 
   candidates = {a:True for a in range(2,max_value+1)} # cardinals up to max_value
   filters = list(range(2,int(max_value**0.5)+1)) # primes up to sqrt(max_value)
+  counter = max_value + int(max_value**0.5)+1 # count of bit operations
 
   for current in filters:
     if candidates[current]:
       runner = current**2
+      counter+=1
+      print("candidate ", current, "counter ", counter)
       while runner <= max_value:
+        counter += 1
         if candidates[runner]:
+          counter += 1
           candidates[runner] = False
+          print("candidate ", current, "composite", runner, "counter ", counter)
         runner += current 
 
   primes = [k for (k,v) in candidates.items() if v == True]
+  counter += len(primes)
+  print("candidate ", current, "counter ", counter)
   return primes
 
-print(find_primes(10))
+print(find_primes(100))
 # assert find_primes(50) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
