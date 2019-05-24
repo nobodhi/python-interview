97a90ade70049c231e09335377ae77f794542c4c
Joseph Brown <josebrwn@gmail.com>
2019-05-24 12:47:01 -0700

    sieve cleanup. try a wheel sieve next.

--- project/sieve_of_eratosthenes.py
+++ project/sieve_of_eratosthenes.py
@@ -1,6 +1,8 @@
 # sieve of eratosthenes O(n log log n)
 
 def find_primes(max_value):
+  """naive solution. check every cardinal number for prime multiples.
+  TODO: a wheel sieve only looks at those cardinals ending in 1,3,7,9"""
 
   candidates = {a:True for a in range(2,max_value+1)} # cardinals up to max_value
   filters = list(range(2,int(max_value**0.5)+1)) # primes up to sqrt(max_value)
@@ -10,18 +12,16 @@ def find_primes(max_value):
     if candidates[current]:
       runner = current**2
       counter+=1
-      print("candidate ", current, "counter ", counter)
       while runner <= max_value:
-        counter += 1
         if candidates[runner]:
           counter += 1
           candidates[runner] = False
-          print("candidate ", current, "composite", runner, "counter ", counter)
         runner += current 
+        counter += 1
 
   primes = [k for (k,v) in candidates.items() if v == True]
   counter += len(primes)
-  print("candidate ", current, "counter ", counter)
+  print("max ", max_value, "operations ", counter)
   return primes
 
 print(find_primes(100))
