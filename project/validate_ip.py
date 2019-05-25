f31c68095ddee39c139fbe007b89dd87e9007161
Joseph Brown <josebrwn@gmail.com>
2019-05-25 09:37:53 -0700

    functional approaches to ip validation

--- project/validate_ip.py
+++ project/validate_ip.py
@@ -1,23 +1,58 @@
 # confirm that a given IP address is valid.
 
+# naive solution
 def validate_ip(ip_address):
   """An IP address consists of 32 bits, shown as 4 terms 
   of numbers from 0-255 represented in decimal form """
 
   terms = ip_address.split(".")
   if len(terms) != 4:
-    return None
+    return False
   for octet in range(0,4):
     if not terms[octet].isdecimal():
-      return None
+      return False
     elif (int(terms[octet]) < 0 or int(terms[octet]) > 255):
-      return None
-  return terms
+      return False
+  return True
 
 print(validate_ip('127.0.0.1'))
 print(validate_ip('127.0.0.1.4'))
 print(validate_ip('127.b.0.1'))
 
-assert validate_ip('127.0.0.1') == ['127', '0', '0', '1']
-assert validate_ip('127.0.0.256') == None
-assert validate_ip('127.0.a.1') == None
+assert validate_ip('127.0.0.1') == True
+assert validate_ip('127.0.0.256') == False
+assert validate_ip('127.0.a.1') == False
+
+# filter function
+def filter_octects(ip_address):
+  """functional approach filters the list"""
+  terms = ip_address.split(".")
+  if not len([*filter(lambda octet: octet.isdecimal(), terms)])==4:
+    return False
+  elif not len([*filter(lambda octet: 0<=int(octet)<=255, terms)])==4:
+    return False
+  else:
+    return True
+
+print(filter_octects('127.0.0.1'))
+
+assert filter_octects('127.0.0.1') == True
+assert filter_octects('127.0.0.256') == False
+assert filter_octects('127.0.a.1') == False
+
+# list comprehension (best!)
+def all_valid(ip_address):
+  """`all` uses list comprehension to filter"""
+  terms = ip_address.split(".")
+  if not all(octet.isdecimal() for octet in terms):
+    return False
+  elif not all(0<=int(octet)<=255 for octet in terms):
+    return False
+  else:
+    return True
+
+print(all_valid('127.0.0.1'))
+
+assert all_valid('127.0.0.1') == True
+assert all_valid('127.0.0.256') == False
+assert all_valid('127.0.a.1') == False
