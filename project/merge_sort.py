5c08c1ae5b2974755f27cf5fcf54f116fa4a3754
Joseph Brown <josebrwn@gmail.com>
2019-06-03 14:32:31 -0700

    improved merge sort

--- project/merge_sort.py
+++ project/merge_sort.py
@@ -1,5 +1,6 @@
 # merge sort
 # h/t https://www.thecrazyprogrammer.com/2017/12/python-merge-sort.html
+# h/t https://www.youtube.com/watch?v=Nso25TkBsYI
 
 def merge_sort(array):
     n = len(array)
@@ -17,29 +18,22 @@ def merge_sort(array):
     
         merge(left,right,array)
 
-# overwrites array
 def merge(left, right, result):
-    i = 0 # left pointer
-    j = 0 # right pointer
-    k = 0 # result pointer
-
-    while (i<len(left) and j<len(right)):
-        if(left[i]<right[j]):
-            result[k] = left[i]
-            i+=1
+    i = j = 0
+    for k in range(0, len(result)):
+        if j >= len(right):
+            result[k:len(result)] = left[i:len(left)]
+            break
+        elif i >= len(left):
+            result[k:len(result)] = right[j:len(left)]
+            break
+        elif left[i] < right[j]:
+                result[k] = left[i]
+                i+=1
         else:
             result[k] = right[j]
             j+=1
-        k+=1
-    while(i<len(left)):
-        result[k] = left[i]
-        i+=1
-        k+=1
-    while(j<len(right)):
-        result[k] = right[j]
-        j+=1
-        k+=1
 
-array = [99,21,3,19]
+array = [99,21,3,19,21,5,67,98,3,12,22,31,45]
 merge_sort(array)
 print(array)
