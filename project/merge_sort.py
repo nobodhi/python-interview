590410ac588ee480d7f2366577fc43c453dec60e
Joseph Brown <josebrwn@gmail.com>
2019-06-06 06:49:13 -0700

    cleaned up variable names

--- project/merge_sort.py
+++ project/merge_sort.py
@@ -14,20 +14,21 @@ def merge_sort(array):
         merge(left,right,array)
 
 def merge(left, right, array):
-    l_ptr = r_ptr = 0
-    for arr_ptr in range(0, len(array)):
-        if r_ptr == len(right):
-            array[arr_ptr:len(array)] = left[l_ptr:len(left)]
+    array_length = len(array)
+    left_index = right_index = 0
+    for array_index in range(0, array_length):
+        if right_index == len(right):
+            array[array_index:array_length] = left[left_index:len(left)]
             break
-        elif l_ptr == len(left):
-            array[arr_ptr:len(array)] = right[r_ptr:len(right)]
+        elif left_index == len(left):
+            array[array_index:array_length] = right[right_index:len(right)]
             break
-        elif left[l_ptr] <= right[r_ptr]:
-                array[arr_ptr] = left[l_ptr]
-                l_ptr+=1
+        elif left[left_index] <= right[right_index]:
+                array[array_index] = left[left_index]
+                left_index+=1
         else:
-            array[arr_ptr] = right[r_ptr]
-            r_ptr+=1
+            array[array_index] = right[right_index]
+            right_index+=1
 
 array = [99,2,3,3,12,4,5]
 l = len(array)
