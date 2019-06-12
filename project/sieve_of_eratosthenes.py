e78d520074403f937d1c59044101ddc24d3be270
Joseph Brown <josebrwn@gmail.com>
2019-06-12 10:24:09 -0700

    ideomatic sieve of eratosthenes

--- project/sieve_of_eratosthenes.py
+++ project/sieve_of_eratosthenes.py
@@ -2,14 +2,14 @@
 
 
 def find_primes(max_value):
-    """naive solution. check every cardinal number for prime multiples.
-    TODO: a wheel sieve only looks at those cardinals ending in 1,3,7,9"""
+    """naive solution. check every cardinal number for prime multiples."""
 
     # cardinals up to max_value
     candidates = {a: True for a in range(2, max_value+1)}
-    sieves = list(filter(lambda f: f <= max_value**0.5, candidates))
+    # sieve = list(filter(lambda f: f <= max_value**0.5, candidates))
+    sieve = [candidate for candidate in candidates if candidate <= max_value**0.5]
 
-    for current in sieves:
+    for current in sieve:
         if candidates[current]:
             runner = current**2
             while runner <= max_value:
@@ -22,4 +22,4 @@ def find_primes(max_value):
 
 
 print(find_primes(100))
-# assert find_primes(50) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+assert find_primes(50) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
