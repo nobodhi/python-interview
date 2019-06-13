744c29e0d571d27f3b1356fdfdde1ebf01d208d4
Joseph Brown <josebrwn@gmail.com>
2019-06-12 19:14:57 -0700

    determine if two lists are rotations of each other

--- project/count_dictionary.py
+++ project/count_dictionary.py
@@ -42,7 +42,7 @@ print(dict2)
 dict3 = {**dict1, **dict2}
 print(dict3)
 
-# more ideomatic method using builtin fuction GOOD
+# more idiomatic method using builtin fuction GOOD
 dict1 = {k: True for k in range(10)}
 print(dict1)
 dict2 = {k: True for k in range(10, 20)}
@@ -50,10 +50,3 @@ print(dict2)
 dict1.update(dict2)
 print(dict1)
 
-# find the most common value of a list using a dictionary 
-# and list comprehension
-def get_most_common(numbers: [int]):
-    result = {x: numbers.count(x) for x in numbers}
-    return max(result, key=result.get)
-
-print(get_most_common([1,3,1,3,2,1]))
