016d9897b217f2cf0356418a9b56b9cf8539ade1
Joseph Brown <josebrwn@gmail.com>
2019-06-12 21:45:32 -0700

    added udemy_course.py, added one_edit_away

--- project/string_problems.py
+++ project/string_problems.py
@@ -1,10 +1,61 @@
 # string problems
 
 
-def is_unique(str):
+def is_unique_words(str):
     """returns True if every word in the string is unique"""
     s = str.split()
     for x in set(s):
         if (s.count(x) > 1):
             return False
     return True
+
+
+def is_unique_chars(str):
+    """returns True if every character in the string is unique"""
+    s = [char for char in str]
+    print(s, str)
+    for x in set(s):
+        if (s.count(x) > 1):
+            print(x)
+            return False
+    return True
+
+
+print(is_unique_chars("abba"))
+print(is_unique_chars("cat"))
+
+
+def one_edit_away(word1, word2) -> bool:
+    """walk thru both char lists and count differences"""
+    list1 = [char for char in word1]
+    list2 = [char for char in word2]
+    print(list1, list2)
+    len1 = len(list1)
+    len2 = len(list2)
+    same_length = (len1 == len2)
+    if abs(len1-len2) > 1:
+        return False
+    differences = 0
+    index1 = index2 = 0
+    while index1 < len1 and index2 < len2:
+        if list1[index1] != list2[index2]:
+            if differences > 0:
+                return False
+            differences += 1
+            if same_length:
+                index1 += 1
+                index2 += 1
+            elif len1 > len2:
+                index1 += 1
+            else:
+                index2 += 1
+        index1 += 1
+        index2 += 1
+    return True
+
+
+
+print(one_edit_away("shell", "shill"))
+print(one_edit_away("shell", "smell"))
+print(one_edit_away("shell", "smells"))
+print(one_edit_away("shell", "smelt"))
---
+++ project/udemy_course.py
@@ -0,0 +1,72 @@
+# udemy course 11-essential-coding-interview-questions
+
+# find the most common value of a list 
+# using a dictionary and list comprehension
+def get_most_common(numbers: [int]):
+    if len(numbers) == 0:
+        return None
+    result = {x: numbers.count(x) for x in numbers}
+    return max(result, key=result.get)
+
+# print(get_most_common([1,3,1,3,2,1]))
+# print(get_most_common([]))
+
+# find the common elements of two lists - udemy
+A = [1,3,4,6,7,9]
+B = [1,2,4,5,9,10]
+
+def find_common_elements(list1: [int], list2: [int]):
+    print(list1, list2)
+    return [item for item in list1 if item in list2]
+
+# print(find_common_elements(A, B))
+
+# determine if two lists are rotations of each other
+# assume there are no duplicates
+def is_rotation(list1, list2):
+    length = len(list1)
+    if length != len(list2):
+        print(list1, list2, "\nno match")
+        return False
+    temp = list1[:] + list1[:]
+    print(list1, list2, temp)
+    for index in range(length):
+        if temp[index:index+length] == list2:
+            print("match!")
+            return True
+    print("no match")
+    return False
+
+list1 = [1, 2, 3, 4, 5, 6, 7]
+list2b = [4, 5, 6, 7, 1, 2, 3]
+is_rotation(list1, list2b) # True.
+list2a = [4, 5, 6, 7, 8, 1, 2, 3]
+is_rotation(list1, list2a) # False.
+list2c = [4, 5, 6, 9, 1, 2, 3]
+is_rotation(list1, list2c) # False.
+list2d = [4, 6, 5, 7, 1, 2, 3]
+is_rotation(list1, list2d) # False.
+list2e = [4, 5, 6, 7, 0, 2, 3]
+is_rotation(list1, list2e) # False.
+list2f = [1, 2, 3, 4, 5, 6, 7]
+is_rotation(list1, list2f) # True.
+list2g = [7, 1, 2, 3, 4, 5, 6]
+is_rotation(list1, list2g) # True.
+
+# NB always check equal length
+# 
+# alternate method:
+# [1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7] # represents any feasible rotation
+# [.........4, 5, 6, 7, 1, 2, 3] True
+# [..................7, 1, 2, 3, 4, 5, 6] True
+# [.........4, 5, 6, 9, 1, 2, 3] False
+# [   2, 3, 4, 5, 6, 7, 1] True
+#
+# accepted method:
+# iterate thru first list, find the first match in the second
+# compare second list's "trailing" sublist with first list's "opening" sublist
+# compare second list's "opening" sublist with first list's "trailing" sublist
+# if they both match, then get_rotation == True
+# e.g.
+# [4, 5, 6, 7], [1, 2, 3]
+
