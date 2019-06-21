a79cc7b4d1493f33e163f7aecf489a91727690ee
Joseph Brown <josebrwn@gmail.com>
2019-06-20 21:06:33 -0700

    in place 2D array rotation

--- project/udemy_course2.py
+++ project/udemy_course2.py
@@ -61,15 +61,34 @@ a2 = [[1, 2, 3, 4],
 
 
 def rotate_in_place(given_array, n):
-    temp = []
-    for loop in range(n//2-1):
-        for index in range(n-loop):
-            print("top->right")
-            print("source", given_array[loop][index+loop])
-            print("target1", given_array[index+loop][n-loop-1])
-            # TODO last value goes into temp
-
+    temp = passes = 0
+    print(to_string(given_array))
+    for loop in range(n//2):
+        passes += 1
+        for index in range(0,n-loop-passes):
+            temp = given_array[loop][loop+index]
+            # top -> right
+            given_array[loop+index][n-loop-1], temp = temp, given_array[loop+index][n-loop-1]
+            # right -> bottom
+            temp, given_array[n-loop-1][n-loop-1-index] = given_array[n-loop-1][n-loop-1-index], temp
+            # bottom -> left
+            temp, given_array[n-loop-1-index][loop] = given_array[n-loop-1-index][loop], temp
+            # left -> top
+            temp, given_array[loop][loop+index] = given_array[loop][loop+index], temp
+            # print(to_string([x[loop:n-loop] for x in given_array[loop:n-loop]]))
 
     return given_array
 
-print(to_string(rotate_in_place(a2, 4))) # should return:
+# print(to_string(rotate_in_place(a2, 4))) # should return:
+
+a3 = [[0, 1, 2, 3, 4, 5, 6, 7, 8],
+[9, 10, 11, 12, 13, 14, 15, 16, 17],
+[18, 19, 20, 21, 22, 23, 24, 25, 26],
+[27, 28, 29, 30, 31, 32, 33, 34, 35],
+[36, 37, 38, 39, 40, 41, 42, 43, 44],
+[45, 46, 47, 48, 49, 50, 51, 52, 53],
+[54, 55, 56, 57, 58, 59, 60, 61, 62],
+[63, 64, 65, 66, 67, 68, 69, 70, 71],
+[72, 73, 74, 75, 76, 77, 78, 79, 80]]
+
+print(to_string(rotate_in_place(a3, 9)))
