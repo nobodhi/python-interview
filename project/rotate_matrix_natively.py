ee6438a0b829d6b522c351a72762e4ec1edefc3b
Joseph Brown <josebrwn@gmail.com>
2019-06-27 20:48:43 -0700

    rotate matrix code cleanup

--- project/rotate_matrix_natively.py
+++ project/rotate_matrix_natively.py
@@ -19,7 +19,6 @@ def to_string(given_array):
 # n = # rows = # columns in the given 2d array
 
 def rotate_in_place(given_array, n):
-    temp = []
     print(to_string(given_array))
     for loop in range(n//2):
         # we need to track an additional loop because our square shrinks from both ends!
@@ -33,19 +32,14 @@ def rotate_in_place(given_array, n):
             temp, given_array[n-loop-1-index][loop] = given_array[n-loop-1-index][loop], temp
             # left -> top
             temp, given_array[loop][loop+index] = given_array[loop][loop+index], temp
-            # print(to_string([x[loop:n-loop] for x in given_array[loop:n-loop]]))
 
     return given_array
 
 print(to_string(rotate_in_place(rl.sequential_array(10), 10))) 
 
-# TODO deprecate n variable. why is this even here??
-
 def rotate_by_vectors(given_array, n):
     """rotate using 4 temp vectors. this is nice if you want to rotate back and forth."""
     print(to_string(given_array))
-
-    # outer loop here, this will count which "layer" of the onion we are in
     for loop in range(n//2):
 
         top = [x[loop:n-loop] for x in given_array[loop:loop+1]][0]
@@ -53,14 +47,18 @@ def rotate_by_vectors(given_array, n):
         left = [x[loop] for x in reversed(given_array[loop:n-loop])] # !
         right = [x[n-loop-1] for x in given_array[loop:n-loop]]
 
-        for index in range(len(left)): # left->top
-            given_array[loop][index] = left[index]
-        for index in range(len(top)): # top->right
+        # top->right
+        for index in range(len(top)): 
             given_array[loop+index][n-loop-1] = top[index]
-        for index in range(len(right)): # right->bottom
+        # right->bottom
+        for index in range(len(right)): 
             given_array[n-loop-1][n-loop-index-1] = right[index]
-        for index in range(len(top)): # bottom->left
+        # bottom->left
+        for index in range(len(top)): 
             given_array[loop+index][loop] = bottom[index]
+        # left->top
+        for index in range(len(left)): 
+            given_array[loop][index] = left[index]
 
     return given_array
 
