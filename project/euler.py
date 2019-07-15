35b3b8cd4111ad904a5acfe722de5aedba507c0e
Joseph Brown <josebrwn@gmail.com>
2019-07-14 21:21:38 -0700

    euler problem 2

--- project/euler.py
+++ project/euler.py
@@ -2,13 +2,14 @@
 
 # https://projecteuler.net/problem=1
 # find the sum of all positive integers less than 1000 that are multiples of 3 or 5
-
 def sum_mulitples(x, y, n):
     """find the sum of all multiples of x and y less than n"""
     return sum([(num % x == 0 or num % y == 0)*num  for num in range(1,n)])
 
 print(sum_mulitples(3, 5, 1000))
 
+# https://projecteuler.net/problem=2
+# find the sum of the even-valued terms in the Fibonacci sequence whose values do not exceed four million.
 def sum_fibonacci(n):
     """sum of the even-valued terms of the fibonacci sequence not exceeding n"""
     if n < 2:
