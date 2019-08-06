3272ecfda4a8732dde4e0b2b10373e77bb246bef
Joseph Brown <josebrwn@gmail.com>
2019-08-06 11:26:12 -0700

    fixed SCM iteration for n>4

--- project/euler.py
+++ project/euler.py
@@ -82,25 +82,25 @@ print('largest_palindrome_number', largest_palindrome_number(100, 1000))
 # only consider numbers ending in 0 beyond n = 4
 def smallest_common_multiple(n: int) -> int:
     """finds the smallest number that divides everything from 1 to n"""
-    # outer loop: iterate index upwards searching for the SCM
     if n <= 2:
         return n
     current = 2*n
+    # outer loop: iterate "current" upwards searching for the SCM
     while True:
         if n > 4 and current % 10 != 0:
+            current = n//10 * 10 + 10
+        # inner loop: iterate "check_num" checking for divisibility
+        for check_num in range(3, n+1):
+            # print(current, check_num)
+            if current % check_num != 0:
+                break
+            else:
+                if check_num == n:
+                    return current
+        if n <= 4:
             current+=2
         else:
-            for index in range(3, n+1):
-                # print(current, index)
-                if current % index != 0:
-                    break
-                else:
-                    if index == n:
-                        return current
-            if n <= 4:
-                current+=2
-            else:
-                current+=10
+            current+=10
 
 print('smallest_common_multiple', smallest_common_multiple(10))
 # print(smallest_common_multiple(20)) 
